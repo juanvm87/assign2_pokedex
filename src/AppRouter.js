9ce2eb5a8686f2a6cdc01e56fd404b313babf30d
Franciszek Nurkiewicz <318212@via.dk>
2023-04-17 20:34:54 +0200

    pokemon card

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<HomePage />} />
+        <Route path="search" element={<SearchPage />} />
       </Route>
-
+      
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { PokemonContext } from "../context/PokemonProvider";
+import { useLocation } from "react-router-dom";
+import CardPokemon from "../components/CardPokemon";
+
+
+const SearchPage = () => {
+  const location = useLocation();
+
+  const { allPokemons, loading } = useContext(PokemonContext);
+
+  const filteredPokemons = allPokemons.filter((pokemon) =>
+    pokemon.name.includes(location.state)
+  );
+
+  return (
+    <div className="container">
+      <p className="p-search">
+        <span>{filteredPokemons.length}</span> Pokemon found
+      </p>
+      
+        <div className="card-list-pokemon container">
+          {filteredPokemons.map((pokemon) => (
+            <CardPokemon pokemon={pokemon} key={pokemon.name} />
+          ))}
+        </div>
+      
+    </div>
+  );
+};
+
+export default SearchPage;
